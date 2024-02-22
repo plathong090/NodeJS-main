c478072f11c0a9eb08b96e8328b333d6d5ac7e43
Prangthong <prangthong250947@gmail.com>
2024-02-22 15:07:37 +0700

    dg

---
+++ src/MongoDB.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
+mongoose.connect(
+    "", {
+        useNewUrlParser: true,
+        useUnitfiedTopology: true,
+    }
+)
+
+const Book = mongoose.model("Book", {
+    id: {
+        type: Number,
+        unique: true,
+        required: true,
+    },
+    title: String,
+    author: String,
+})
+
+const app = express()
+app.use(bodyParser.json())
+
+
+app.post('/books', async(req, res) => {
+    try {
+
+        const lastBook = await Book.findOne().Sort({ id: -1 })
+        const nextId = lastBook ? lastBook.id + 1 : 1
+
+
+        const book = new Book({
+            id: nextId,
+            ...req.body,
+        })
+
+        await book.save()
+        res.send(book)
+    } catch (err) {
+        res.status(500).send('Error')
+    }
+})
+
+
+app.get('/books', async(req, res) => {
+    try {
+        const books = await Book.find()
+        res.send(books)
+    } catch (err) {
+        res.status(500).send('Error')
+    }
+})
+
+
+app.get('/books/:id', async(req, res) => {
+    try {
+        const book = await Book.findOne({ id: req.params.id })
+        res.send(books)
+    } catch (err) {
+        res.status(500).send('Error')
+    }
+})
+
+
+app.put('/books/:id', async(req, res) => { // show create desktop
+    try {
+        const book = await Book.findOneAndUpdate({ id: req.params.id }, req.body, {
+            new: true,
+        })
+        res.send(books)
+    } catch (err) {
+        res.status(500).send('Error')
+    }
+})
+
+
+app.delete('/books/:id', async(req, res) => {
+    try {
+        const book = await Book.findOneAndDelete({ id: req.params.id })
+        res.send(books)
+    } catch (err) {
+        res.status(500).send('Error')
+    }
+})
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server Started at http://localhost${port}`))
